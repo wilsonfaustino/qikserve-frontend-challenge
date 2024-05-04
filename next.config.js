cdc7482bd2793caae6a5242c80641b4f072cead6
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-04 19:15:23 -0300

    feat: Add lazy loading for remote images in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'preodemo.gumlet.io',
+        port: '',
+        pathname: '/usr/venue/**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
