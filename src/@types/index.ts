f0abfb56687cc2f69f4c5ef1212462374c795ce2
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-05 19:55:27 -0300

    refactor: Update @types/index.ts to include Category and Product types

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,8 +1,13 @@
 import type { z } from 'zod'
 
-import type { contentSchema } from '~/schemas/content'
+import type { contentSchema, itemSchema, sectionSchema } from '~/schemas/content'
 import type { settingsSchema } from '~/schemas/settings'
 
 export type Settings = z.infer<typeof settingsSchema>['webSettings']
+export type Locale = Pick<z.infer<typeof settingsSchema>, 'locale' | 'ccy' | 'timezone' | 'timezoneOffset'>
 
 export type Products = z.infer<typeof contentSchema>
+
+export type Category = z.infer<typeof sectionSchema>
+
+export type Product = z.infer<typeof itemSchema>
