5915bd4a8f4a83ff186e12e3cecf309fd8b609eb
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-05 10:39:12 -0300

    refactor: Update Main component to use children prop for content rendering

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,27 +1,13 @@
-import type { Content } from '~/@types'
-
-import { Cart } from '../cart'
+import type { ReactNode } from 'react'
 
 export type Props = {
-  content: Content
+  children: ReactNode
 }
 
-export const Main = ({ content }: Props) => {
+export const Main = ({ children }: Props) => {
   return (
-    <main className="mx-auto flex w-full max-w-5xl flex-col items-center justify-center bg-red-500 text-center">
-      <div className="h-14 w-full bg-white"></div>
-      <div className="bg-app-400 flex w-full gap-6 px-10 py-8">
-        <div className="h-96 w-[600px] bg-yellow-400">
-          <ul>
-            {content.sections.map((section) => (
-              <li key={section.id}>
-                <h2>{section.name}</h2>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <Cart />
-      </div>
+    <main className="bg-app-400 mx-auto flex w-full max-w-5xl flex-col items-center justify-center text-center">
+      {children}
     </main>
   )
 }
