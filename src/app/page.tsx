f316554d80a90268f8d9ed748cc42fd8ad245261
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-05 10:39:23 -0300

    refactor: Update Home component to include SearchBar, ProductList, and Cart components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from 'next'
 
 import { getContent } from '~/api/content'
+import { Cart } from '~/components/cart'
 import { Main } from '~/components/main'
+import { ProductList } from '~/components/product-list'
+import { SearchBar } from '~/components/search-bar'
 import { env } from '~/env'
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -16,8 +19,12 @@ export default async function Home() {
   const content = await getContent()
 
   return (
-    <>
-      <Main content={content} />
-    </>
+    <Main>
+      <SearchBar />
+      <div className="flex w-full gap-6 px-10 py-8">
+        <ProductList products={content} />
+        <Cart />
+      </div>
+    </Main>
   )
 }
