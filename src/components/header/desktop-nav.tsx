2f5115c25cac17f6e6813087094cee071c4e35ca
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-05 16:10:00 -0300

    refactor: Update DesktopNav component to use settings object for background color

--- src/components/header/desktop-nav.tsx
+++ src/components/header/desktop-nav.tsx
@@ -1,10 +1,26 @@
+'use client'
+
+import { useEffect } from 'react'
+import { useSetRecoilState } from 'recoil'
+
+import type { Settings } from '~/@types'
+import { settingsState } from '~/context/settings-atom'
+
 import { NavLink } from './nav-link'
 
 export type Props = {
-  backgroundColor: string
+  settings: Settings
 }
 
-export function DesktopNav({ backgroundColor }: Props) {
+export function DesktopNav({ settings }: Props) {
+  const setSettings = useSetRecoilState(settingsState)
+  const backgroundColor = settings?.navBackgroundColour || 'black'
+
+  useEffect(() => {
+    if (settings) {
+      setSettings(settings)
+    }
+  }, [settings, setSettings])
   return (
     <div className="flex h-[52px] w-full items-center justify-center" style={{ backgroundColor }}>
       <nav className="flex w-full max-w-[1440px] items-center justify-center p-4 text-white">
