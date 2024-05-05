b4982b4c5c56ea4ea098244fd797c822c9068625
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-05 08:48:47 -0300

    feat: Add Content type to index.ts

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,5 +1,8 @@
 import type { z } from 'zod'
 
+import type { contentSchema } from '~/schemas/content'
 import type { settingsSchema } from '~/schemas/settings'
 
 export type Settings = z.infer<typeof settingsSchema>['webSettings']
+
+export type Content = z.infer<typeof contentSchema>
