ff658e378c92bc1b01cfa65560d52b50a2d77008
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-05 13:22:00 -0300

    feat: Add search icon SVG and integrate it into the SearchBar component

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,3 +1,29 @@
+import Image from 'next/image'
+
+import searchIcon from '~/assets/search-icon.svg'
+import { cn } from '~/utils/classnames'
+
 export function SearchBar() {
-  return <div className="bg-app-500 flex h-14 w-full items-center justify-center">SEARCH BAR</div>
+  return (
+    <div className="bg-app-500 flex h-14 w-full items-center justify-center py-1.5">
+      <div
+        className={cn(
+          'ring-app-600 flex h-11 w-full items-center justify-center rounded-lg bg-white ring-1 ring-inset transition',
+          'focus-within:ring-2 focus-within:ring-inset',
+        )}
+      >
+        <div className="flex h-11 w-12 items-center justify-center">
+          <Image src={searchIcon} alt="Search icon" width={24} height={24} />
+        </div>
+        <input
+          type="text"
+          className={cn(
+            'h-11 w-full border-transparent bg-transparent',
+            'placeholder:text-app-700 focus:border-transparent focus:outline-0 focus:ring-0',
+          )}
+          placeholder="Search menu items"
+        />
+      </div>
+    </div>
+  )
 }
