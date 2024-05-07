943ab87866ff13710a9e0bd86a1f29cfb543bdf9
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-07 14:57:23 -0300

    refactor: Update CategoryList component to filter items based on search term

--- src/components/category-list/category-list.tsx
+++ src/components/category-list/category-list.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import Image from 'next/image'
+import { useRecoilValue } from 'recoil'
 
 import type { Category } from '~/@types'
 import arrowUp from '~/assets/arrow-up.svg'
+import { searchTermState } from '~/context/search-atom'
 
 import { ProductItem } from './product-item'
 
@@ -10,21 +14,28 @@ export type Props = {
 }
 
 export function CategoryList({ category }: Props) {
+  const searchTerm = useRecoilValue(searchTermState)
+  const filteredItems = searchTerm
+    ? category.items.filter((item) => item.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    : category.items
+
   return (
-    <div className="w-full">
-      <div className="flex h-full w-full items-center justify-between px-4 pb-3 pt-8">
-        <h2 id={category.name} className="text-2xl font-medium">
-          {category.name}
-        </h2>
-        <a href="">
-          <Image src={arrowUp} alt="arrow up" width={24} height={24} />
-        </a>
-      </div>
+    <li className="w-full">
+      {filteredItems.length > 0 && (
+        <div className="flex h-full w-full items-center justify-between px-4 pb-3 pt-8">
+          <h2 id={category.name} className="text-2xl font-medium">
+            {category.name}
+          </h2>
+          <a href="#top">
+            <Image src={arrowUp} alt="arrow up" width={24} height={24} />
+          </a>
+        </div>
+      )}
       <div className="flex w-full flex-col overflow-x-auto">
-        {category.items.map((item) => (
+        {filteredItems.map((item) => (
           <ProductItem key={item.id} product={item} />
         ))}
       </div>
-    </div>
+    </li>
   )
 }
