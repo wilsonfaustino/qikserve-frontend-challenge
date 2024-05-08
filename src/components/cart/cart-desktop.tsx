17f234e64c1a48f4419e1ed309f0a7b5383c8871
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-08 17:00:18 -0300

    refactor: Update cart component to use responsive layout for mobile and desktop devices

---
+++ src/components/cart/cart-desktop.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import { CountPicker } from '../count-picker'
+import { useCart } from './hooks/cart'
+
+export function CartDesktop() {
+  const { cartItems, cartTotal, formatPrice, handleQuantity, styleSettings, subTotal } = useCart()
+
+  if (cartItems.length === 0) {
+    return (
+      <div className="h-full min-h-[129px] w-80 bg-app-400 shadow-lg">
+        <div className="flex h-16 items-center px-6">
+          <h2 className="text-2xl font-medium text-app-800">Carrinho</h2>
+        </div>
+        <div className="flex h-16 items-center bg-white px-6">
+          <p className="text-app-800">Seu carrinho está vazio</p>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="h-full min-h-[129px] w-[320px] overflow-hidden bg-app-400 shadow-lg">
+      <div className="flex h-16 items-center px-6">
+        <h2 className="text-2xl font-medium text-app-800">Carrinho</h2>
+      </div>
+      <div className="flex w-full flex-col bg-white shadow-sm">
+        {cartItems.map((item) => (
+          <div key={`${item.id}-${Math.random()}`} className="px-4 pb-4 pt-2 text-left">
+            <div className="flex items-center justify-between">
+              <h3>{item.name}</h3>
+              <p>{formatPrice(item.price * item.quantity)}</p>
+            </div>
+            {item.modifiers && (
+              <ul>
+                {item.modifiers.map((modifier) => (
+                  <li key={modifier.id} className="opacity-50">
+                    {modifier.name}
+                  </li>
+                ))}
+              </ul>
+            )}
+            <CountPicker
+              baseColor={styleSettings?.primaryColour}
+              size="sm"
+              value={item.quantity}
+              onChange={(val) => handleQuantity(item, val)}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="flex h-16 items-center justify-between border-b-2 px-6">
+        <p>Subtotal</p>
+        <p>{formatPrice(subTotal)}</p>
+      </div>
+      <div className="flex h-16 items-center justify-between px-6 text-2xl">
+        <p>Total:</p>
+        <p className="font-bold">{formatPrice(cartTotal)}</p>
+      </div>
+    </div>
+  )
+}
