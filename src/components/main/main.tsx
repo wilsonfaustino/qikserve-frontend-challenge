597451ba5fe9cd2fc695531645cc587376ca6518
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-08 20:50:18 -0300

    refactor: Update main component to include bottom padding for better spacing

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -6,7 +6,7 @@ export type Props = {
 
 export const Main = ({ children }: Props) => {
   return (
-    <main className="mx-auto flex w-full max-w-5xl flex-col items-center justify-center bg-app-400 text-center">
+    <main className="mx-auto flex w-full max-w-5xl flex-col items-center justify-center bg-app-400 pb-20 text-center">
       {children}
     </main>
   )
