d68a7d61a7f24ae86ebc81aa6baddf091d85c825
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-08 12:03:52 -0300

    refactor: Update useDevice hook to adjust breakpoints for mobile and desktop devices

--- src/hooks/device.ts
+++ src/hooks/device.ts
@@ -1,8 +1,8 @@
 import { useMediaQuery } from '@react-hookz/web'
 
 export function useDevice() {
-  const isMobile = useMediaQuery('(max-width: 1023px)', { initializeWithValue: false })
-  const isDesktop = useMediaQuery('(min-width: 1024px)', { initializeWithValue: false })
+  const isMobile = useMediaQuery('(max-width: 768px)', { initializeWithValue: false })
+  const isDesktop = useMediaQuery('(min-width: 769px)', { initializeWithValue: false })
 
   return { isMobile, isDesktop }
 }
