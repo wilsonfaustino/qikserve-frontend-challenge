0c85265dfbe93b249000204bd25b808a038f2d5d
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-08 12:02:39 -0300

    refactor: Update DesktopNav component to accept dynamic links and background color

--- src/components/header/desktop-nav.tsx
+++ src/components/header/desktop-nav.tsx
@@ -1,32 +1,20 @@
-'use client'
-
-import { useEffect } from 'react'
-import { useSetRecoilState } from 'recoil'
-
-import type { Settings } from '~/@types'
-import { settingsState } from '~/context/settings-atom'
-
+import type { Link } from './display-nav'
 import { NavLink } from './nav-link'
 
 export type Props = {
-  settings: Settings
+  backgroundColor?: string
+  links: Link[]
 }
 
-export function DesktopNav({ settings }: Props) {
-  const setSettings = useSetRecoilState(settingsState)
-  const backgroundColor = settings?.navBackgroundColour || 'black'
-
-  useEffect(() => {
-    if (settings) {
-      setSettings(settings)
-    }
-  }, [settings, setSettings])
+export function DesktopNav({ links, backgroundColor = 'black' }: Props) {
   return (
     <div className="flex h-[52px] w-full items-center justify-center" style={{ backgroundColor }}>
-      <nav className="flex w-full max-w-[1440px] items-center justify-center p-4 text-white">
-        <NavLink href="/">MENU</NavLink>
-        <NavLink href="/signin">ENTRAR</NavLink>
-        <NavLink href="/contact">CONTATO</NavLink>
+      <nav className="flex w-full items-center justify-center p-4 text-white">
+        {links.map((link) => (
+          <NavLink key={link.href} href={link.href}>
+            {link.label}
+          </NavLink>
+        ))}
       </nav>
     </div>
   )
