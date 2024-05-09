7d13c2dc776fdffa08be7e8d6a8b700e01aaf7ed
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-08 22:03:39 -0300

    refactor: Update SVG icons to use camel case for fillRule and clipRule

--- src/components/icons/plus-icon.tsx
+++ src/components/icons/plus-icon.tsx
@@ -15,8 +15,8 @@ export function PlusIcon({ className, accentColor = 'black' }: Props) {
       className={className}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M7.5 16.5C7.5 17.3284 8.17157 18 9 18C9.82843 18 10.5 17.3284 10.5 16.5V10.5H16.5C17.3284 10.5 18 9.82843 18 9C18 8.17157 17.3284 7.5 16.5 7.5H10.5V1.5C10.5 0.671573 9.82843 0 9 0C8.17157 0 7.5 0.671573 7.5 1.5V7.5H1.5C0.671573 7.5 0 8.17157 0 9C0 9.82843 0.671573 10.5 1.5 10.5H7.5V16.5Z"
         fill={accentColor}
       />
