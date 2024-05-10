1e0cfd2cf7f17031c49a5e18e202b7f330bee337
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-10 12:07:54 -0300

    refactor: Update page titles and content in contact and signin pages

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -3,7 +3,10 @@ import { Main } from '~/components/main'
 export default function ContactPage() {
   return (
     <Main>
-      <h1>Contato</h1>
+      <div className="flex h-96 w-full flex-col items-center justify-center gap-4">
+        <h1 className="text-3xl font-bold text-app-800">Contact</h1>
+        <p>Under construction</p>
+      </div>
     </Main>
   )
 }
